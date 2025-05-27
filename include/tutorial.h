9cf222c8ad7c4e229945508a9d045f72acecd0ed
svaniksharma <svaniksharma2@gmail.com>
2025-05-26 21:16:40 -0500

    Added computation with backend

--- include/tutorial.h
+++ include/tutorial.h
@@ -1,5 +1,8 @@
+#include "ggml-alloc.h"
+#include "ggml-backend.h"
 #ifndef TUTORIAL_GGML_H
 #include "ggml.h"
+#include "ggml-cpu.h"
 
 class TutorialRegression {
 public:
@@ -29,6 +32,50 @@ private:
   struct ggml_tensor *_x;
   struct ggml_context *_ctx;
   struct ggml_tensor *_result;
+};
+
+class BackendRegression {
+public:
+  BackendRegression() {
+    struct ggml_init_params params = {
+      1024 * ggml_tensor_overhead(),
+      nullptr,
+      true
+    };
+    _ctx_static = ggml_init(params);
+    _a = ggml_new_tensor_1d(_ctx_static, GGML_TYPE_F32, 1);
+    _b = ggml_new_tensor_1d(_ctx_static, GGML_TYPE_F32, 1);
+    _x = ggml_new_tensor_1d(_ctx_static, GGML_TYPE_F32, 1);
+    ggml_set_param(_x);
+    _backend = ggml_backend_cpu_init();
+    _backend_buffer = ggml_backend_alloc_ctx_tensors(_ctx_static, _backend);
+    params = {
+      ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + 3 * ggml_graph_overhead(),
+      nullptr,
+      true
+    };
+    _ctx_compute = ggml_init(params);
+    struct ggml_tensor *ax = ggml_mul(_ctx_compute, _a, _x);
+    _result = ggml_add(_ctx_compute, ax, _b);
+    ggml_set_output(_result);
+  }
+  void set_params(const float a, const float b);
+  float forward(const float x);
+  ~BackendRegression() {
+    ggml_free(_ctx_static);
+    ggml_free(_ctx_compute);
+    ggml_backend_free(_backend);
+    ggml_backend_buffer_free(_backend_buffer);
+  }
+private:
+  struct ggml_tensor *_a;
+  struct ggml_tensor *_b;
+  struct ggml_tensor *_x;
+  struct ggml_tensor *_result;
+  struct ggml_context *_ctx_static;
+  struct ggml_context *_ctx_compute;
+  struct ggml_backend *_backend;
+  ggml_backend_buffer_t _backend_buffer;
 };
 
 #endif // !TUTORIAL_GGML_H
--- src/tutorial.cpp
+++ src/tutorial.cpp
@@ -1,9 +1,10 @@
 #include "tutorial.h"
+#include "ggml-alloc.h"
 #include "ggml-cpu.h"
 #include "ggml.h"
 #include <iostream>
 
-void TutorialRegression::set_params(float a, float b) {
+void TutorialRegression::set_params(const float a, const float b) {
   ggml_set_f32(_a, a);
   ggml_set_f32(_b, b);
 }
@@ -16,10 +17,31 @@ float TutorialRegression::forward(const float x) {
   return ggml_get_f32_1d(_result, 0);
 }
 
+void BackendRegression::set_params(const float a, const float b) {
+  ggml_set_f32(_a, a);
+  ggml_set_f32(_b, b);
+}
+
+float BackendRegression::forward(const float x) {
+  ggml_set_f32(_x, x);
+  struct ggml_cgraph *cf = ggml_new_graph(_ctx_compute);
+  ggml_build_forward_expand(cf, _result);
+  ggml_gallocr_t allocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(_backend));
+  ggml_gallocr_alloc_graph(allocr, cf);
+  ggml_backend_graph_compute(_backend, cf);
+  float result = ggml_get_f32_1d(_result, 0);
+  ggml_gallocr_free(allocr);
+  return result;
+}
+
 int main (int argc, char *argv[]) {
   TutorialRegression regressor;
   regressor.set_params(3.0f, 4.0f);
   float result = regressor.forward(5.0f);
-  std::cout << "Result: " << result << "\n";
+  std::cout << "Tutorial Result: " << result << "\n";
+  BackendRegression backend_regressor;
+  backend_regressor.set_params(3.0f, 4.0f);
+  result = regressor.forward(5.0f);
+  std::cout << "Backend result: " << result << "\n";
   return 0;
 }
