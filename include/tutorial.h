69c99bc4b6983afb7feffc0da12e922a1a33e7ca
svaniksharma <svaniksharma2@gmail.com>
2025-05-27 21:49:22 -0500

    Added comments and made some fixes

--- include/tutorial.h
+++ include/tutorial.h
@@ -14,18 +14,23 @@ class TutorialRegression {
 public:
   TutorialRegression() {
     struct ggml_init_params params {
-      1024 * ggml_tensor_overhead(), 
-      nullptr,
-      false
+      /* .mem_size = */ 1024 * ggml_tensor_overhead(), 
+      /* .mem_buffer = */ nullptr,
+      /* .no_alloc = */ false
     };
+    /* We initialize a context. 
+     * The context keeps track of the tensors and operations between them. 
+     * In this case, we are also using it to perform the computation. */
     _ctx = ggml_init(params); 
+    // Using the context, we construct the computational graph for y = a * x + b.
+    // Note that no computation is actually being performed.
+    // This is only done to build the graph. 
     _a = ggml_new_tensor_1d(_ctx, GGML_TYPE_F32, 1);
     _b = ggml_new_tensor_1d(_ctx, GGML_TYPE_F32, 1);
     _x = ggml_new_tensor_1d(_ctx, GGML_TYPE_F32, 1);
     ggml_set_param(_x);
     struct ggml_tensor *ax = ggml_mul(_ctx, _a, _x);
     _result = ggml_add(_ctx, ax, _b);
-    ggml_set_output(_result);
   }
   void set_params(const float a, const float b);
   float forward(const float x);
@@ -43,7 +48,8 @@ private:
 template<typename T>
 class DataLoader {
 public:
-  DataLoader(const float matrix[][2], const size_t N) {
+  DataLoader(const T matrix[][2], const size_t N) {
+    // This can work for either double or float.
     enum ggml_type dataset_type;
     if (std::is_same<T, float>::value)
       dataset_type = GGML_TYPE_F32;
@@ -51,7 +57,16 @@ public:
       dataset_type = GGML_TYPE_F64;
     else
       GGML_ASSERT(false);
+    /* This creates a dataset. The arguments are as follows:
+     * type_data: We set this above: 32-bit float or 64-bit float.
+     * type_label: Same as above. In this case, it's the same type.
+     * ne_datapoint: Number of elements per datapoint (i.e, how many features per datapoint) 
+     * ne_label: Number of elements per label (i.e, how many outputs/targets/dependent variables do you have)
+     * ndata: The number of datapoints and labels
+     * ndata_shard: A shard is the unit along which a datapoint is shuffled. This is the number of points per shard. */
     _dataset = ggml_opt_dataset_init(dataset_type, dataset_type, 1, 1, N, 1);
+    // Once the dataset is created, the underlying tensors are allocated for you based on the arguments passed above. ^^^
+    // The following code gets the underlying tensors, and then uses ggml_get_data to get the actual buffer inside of the tensor. We then use the matrix passed in the constructor (first column datapoint, second column labels) to set these underlying buffers.
     struct ggml_tensor *data = ggml_opt_dataset_data(_dataset);
     struct ggml_tensor *labels = ggml_opt_dataset_labels(_dataset);
     T *data_buf = static_cast<T *>(ggml_get_data(data));
@@ -73,11 +88,14 @@ template <typename T>
 class BackendRegression {
 public:
   BackendRegression() {
+    // This can work for either double or float.
+    // Notice that .no_alloc is true here. We want BLAH BLAH
     struct ggml_init_params params = {
-      1024 * ggml_tensor_overhead(),
-      nullptr,
-      true
+      /* .mem_size = */ 1024 * ggml_tensor_overhead(),
+      /* .mem_buffer = */ nullptr,
+      /* .no_alloc = */ true
     };
+    // Like before, we determine whether we are dealing with float or double.
     enum ggml_type tensor_type;
     if (std::is_same<T, float>::value)
       tensor_type = GGML_TYPE_F32;
@@ -85,36 +103,57 @@ public:
       tensor_type = GGML_TYPE_F64;
     else
       GGML_ASSERT(false);
+    // We now allocate a context, but we will be using a backend for computations. 
+    // In this case, the only purpose of the *static* context is to create the tensor metadata.
     _ctx_static = ggml_init(params);
     _a = ggml_new_tensor_1d(_ctx_static, tensor_type, 1);
     _b = ggml_new_tensor_1d(_ctx_static, tensor_type, 1);
     _x = ggml_new_tensor_1d(_ctx_static, tensor_type, 1);
+    // Since we are going to be using this for both training and inference, we need to specify the 
+    // model inputs, outputs, and parameters. In y = a * x + b, a and b are parameters, x is an input.
     ggml_set_input(_x);
     ggml_set_param(_a);
     ggml_set_param(_b);
+    // Now we initialize the backend. In this case, we use CUDA as the backend.
+    // The backend buffer is returned after we allocate the tensors using the static context + backend.
+    // We will need to keep the backend buffer to free it later.
     _backend = ggml_backend_cuda_init(0);
     _backend_buffer = ggml_backend_alloc_ctx_tensors(_ctx_static, _backend);
+    // Now, we create the *compute* context. This is what does inference and training. 
+    // Again, .no_alloc = true because BLAH BLAH. Calculating the memory needed is easy
+    // and we don't have to allocate a sufficiently large amount or check with ggml_mem.
+    // By default, GGML allocates 2048 nodes (GGML_DEFAULT_GRAPH_SIZE) when allocating a graph.
+    // Each of the 2048 nodes carries overhead since they are essentially tensors. Since we are 
+    // doing inference and training, we need to allocate 1 graph for the forward pass, 1 for the backward pass.
     params = {
-      ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + 3 * ggml_graph_overhead(),
+      ggml_tensor_overhead() * GGML_DEFAULT_GRAPH_SIZE + 2 * ggml_graph_overhead(),
       nullptr,
       true
     };
+    // This time, we use the *compute* context in order to construct the computational graph.
+    // Note that after we get `_result`, we use ggml_set_output to mark _result as the output tensor.
     _ctx_compute = ggml_init(params);
     struct ggml_tensor *ax = ggml_mul(_ctx_compute, _a, _x);
     _result = ggml_add(_ctx_compute, ax, _b);
     ggml_set_output(_result);
+    // To do training, we need a backend scheduler. The backend scheduler allows us to manage several backends 
+    // at once for inference and training. In this case, we really only need it to fit the model. We push the CPU
+    // backend as well since it is required as a fallback.
     std::vector<ggml_backend_t> backends;
     backends.push_back(_backend);
     backends.push_back(ggml_backend_cpu_init());
     _backend_sched = ggml_backend_sched_new(backends.data(), nullptr, backends.size(), GGML_DEFAULT_GRAPH_SIZE, false, true);
     std::cout << "Using " << ggml_backend_name(_backend) << " as backend\n";
+    // After constructing the computational graph, we need to allocate the graph.
+    // ggml_gallocr_new needs to know the backend buffer type. In this case, we 
+    // find the backend buffer type using ggml_backend_get_default_buffer_type.
     _gf = ggml_new_graph(_ctx_compute);
     ggml_build_forward_expand(_gf, _result);
-    ggml_gallocr_t allocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(_backend));
-    ggml_gallocr_alloc_graph(allocr, _gf); 
+    _allocr = ggml_gallocr_new(ggml_backend_get_default_buffer_type(_backend));
+    ggml_gallocr_alloc_graph(_allocr, _gf); 
   }
   void set_params(const T a, const T b);
-  float forward(const T x);
+  T forward(const T x);
   void train(const DataLoader<T> &dl);
   void print_params() const;
   ~BackendRegression() {
--- src/tutorial.cpp
+++ src/tutorial.cpp
@@ -8,37 +8,65 @@
 #include <random>
 
 void TutorialRegression::set_params(const float a, const float b) {
+  // This just sets the values of `_a` and `_b`. This is used once we are 
+  // ready to perform the computation (see `forward`).
   ggml_set_f32(_a, a);
   ggml_set_f32(_b, b);
 }
 
 float TutorialRegression::forward(const float x) {
+  // Set the input tensor `_x`
   ggml_set_f32(_x, x);
+  // Create a new graph using the context
   struct ggml_cgraph *cf = ggml_new_graph(_ctx);
+  // Use the new graph and the output tensor to build the graph outwards
   ggml_build_forward_expand(cf, _result);
+  // Use the graph and context to compute the result (use 1 thread)
   ggml_graph_compute_with_ctx(_ctx, cf, 1);
+  // get the result from the tensor (since this tensor holds 1 value, we pass index 0).
   return ggml_get_f32_1d(_result, 0);
 }
 
 template<typename T>
 void BackendRegression<T>::set_params(const T a, const T b) {
+  // Similar to set_params for TutorialRegression. But now, we are using a backend.
+  // We have to use ggml_backend_tensor_set since ggml_set_f32 is used specifically for the CPU backend. 
   ggml_backend_tensor_set(_a, &a, 0, ggml_nbytes(_a));
   ggml_backend_tensor_set(_b, &b, 0, ggml_nbytes(_b));
 }
 
 template <typename T>
-float BackendRegression<T>::forward(const T x) {
+T BackendRegression<T>::forward(const T x) {
+  // Again, we use ggml_backend_tensor_set instead of ggml_set_f32
   ggml_backend_tensor_set(_x, &x, 0, ggml_nbytes(_x));
+  // We already built and allocated the graph in the constructor. 
+  // Now we just have to do the computation using the backend.
   ggml_backend_graph_compute(_backend, _gf);
+  // The result is stored in _result, but this shows an alternate way to get it.
+  // We know that the last node in the graph is the result, we we fetch the result node.
   struct ggml_tensor *result = ggml_graph_node(_gf, -1);
-  float result_data = 0;
+  // Now, we use the backend to get the data.
+  T result_data = 0;
   ggml_backend_tensor_get(result, &result_data, 0, ggml_nbytes(result));
   return result_data;
 }
 
 template<typename T>
 void BackendRegression<T>::train(const DataLoader<T> &dl) {
-  ggml_opt_fit(_backend_sched, _ctx_compute, _x, _result, dl.get_dataset(), GGML_OPT_LOSS_TYPE_MEAN_SQUARED_ERROR, ggml_opt_get_default_optimizer_params, 5, 1, 0.2f, true);
+  /* We train the model on the dataset. Here are the parameters:
+   * backend_sched: The backend scheduler (we made this in the constructor) 
+   * ctx_compute: The compute context (we made this in the constructor, too)
+   * inputs: In this case, it is the tensor, `_x`. 
+   * outputs: In this case, it is the tensor, `_result`. 
+   * dataset: A ggml_opt_dataset_t. We made this inside of the DataLoader `dl`.
+   * loss_type: A loss type (in this case, we use mean squared error). 
+   * get_opt_pars: A callback that returns the ggml_opt_get_optimizer_params. In this case, we use 
+   * the default parameters.
+   * nepoch: The number of epochs.
+   * nbatch_logical: How many values per batch.
+   * val_split: What percentage of the data should we use for validation? We don't really need this in this example. 
+   * silent: do not print diagnostic output to stderr */
+   ggml_opt_fit(_backend_sched, _ctx_compute, _x, _result, dl.get_dataset(), GGML_OPT_LOSS_TYPE_MEAN_SQUARED_ERROR, ggml_opt_get_default_optimizer_params, 5, 1, 0.2f, true);
 }
 
 template<typename T>
@@ -52,28 +80,36 @@ void BackendRegression<T>::print_params() const {
 }
 
 int main (int argc, char *argv[]) { 
+  /* TutorialRegression: Compute a * x + b where a = 3, b = 4, x = 5, (end result should be 19) */
   TutorialRegression regressor;
   regressor.set_params(3.0f, 4.0f);
   float result = regressor.forward(5.0f);
   std::cout << "Tutorial Result: " << result << "\n";
+  /* Do the same thing as TutorialRegression, but with a backend (end result should be 19) */
   BackendRegression<float> backend_regressor;
   backend_regressor.set_params(3.0f, 4.0f);
   result = regressor.forward(5.0f);
   std::cout << "Backend result: " << result << "\n";
+  /* Create 10000 datapoints; first column is x, second column is y. This is our "dataset" */
   const int N = 10000;
   float matrix[N][2];
-  std::memset(matrix, 0, 20 * sizeof(float));
+  // Randomly generate the parameters a and b.
+  std::memset(matrix, 0, 2 * N * sizeof(float));
   std::uniform_real_distribution<float> unif(1, 10);
   std::default_random_engine re;
   double a = unif(re);
   double b = unif(re);
   std::cout << "Parameters to recover: a=" << a << "; b=" << b << "\n";
+  // Compute a * x + b for integer x in the interval [1, N].
   for (int i = 0; i < N; i++) {
     matrix[i][0] = static_cast<float>(i+1);
     matrix[i][1] = a * matrix[i][0] + b;
   }
+  // Use the DataLoader on the matrix to create a GGML dataset.
   DataLoader<float> dl(matrix, N);
+  // Train the backend regressor on the dataset.
   backend_regressor.train(dl);
+  // Print the results, and evaluate at the points x = 15000, x = 20000, x = 30000
   std::cout << "Recovered parameters\n---------------\n";
   backend_regressor.print_params();
   std::cout << "Evaluation on test data\n------------\n";
